cdb462e6e289b968da72ad7a1f93f370cac163f0
taku220@gmail.com <taku220@gmail.com>
2015-08-02 00:05:00 +0900

    README.mdの更新

--- MklinkGUI/MainWindow.xaml.cs
+++ MklinkGUI/MainWindow.xaml.cs
@@ -109,18 +109,12 @@ namespace MklinkGUI
 
         private string GetFolderPath()
         {
-            //FolderBrowserDialogクラスのインスタンスを作成
             System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog();
 
-            //上部に表示する説明テキストを指定する
             fbd.Description = "フォルダを指定してください。";
-            //ルートフォルダを指定する
-            //デフォルトでDesktop
+
             fbd.RootFolder = Environment.SpecialFolder.Desktop;
-            //最初に選択するフォルダを指定する
-            //RootFolder以下にあるフォルダである必要がある
-            //ユーザーが新しいフォルダを作成できるようにする
-            //デフォルトでTrue
+
             fbd.ShowNewFolderButton = true;
 
             //ダイアログを表示する
